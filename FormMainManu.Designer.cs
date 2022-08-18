03c7389af43dfe62bf292c9d081a22c50dec7b10
Jonatas Nascimento <jonatas.nascimento@elisbrasil.onmicrosoft.com>
2022-08-18 18:38:09 -0300

    Add project files.

---
+++ FormMainManu.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace ReaderSettingsApp
+{
+    partial class FormMainManu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.buttonGerarCNX = new System.Windows.Forms.Button();
+            this.buttonConfigurarCNX = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.labelLogo = new System.Windows.Forms.Label();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelDesktopPanel = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.panelTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.panelMenu.Controls.Add(this.buttonGerarCNX);
+            this.panelMenu.Controls.Add(this.buttonConfigurarCNX);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panelMenu.ForeColor = System.Drawing.Color.White;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 450);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // buttonGerarCNX
+            // 
+            this.buttonGerarCNX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
+            this.buttonGerarCNX.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonGerarCNX.FlatAppearance.BorderSize = 0;
+            this.buttonGerarCNX.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGerarCNX.Location = new System.Drawing.Point(0, 133);
+            this.buttonGerarCNX.Name = "buttonGerarCNX";
+            this.buttonGerarCNX.Size = new System.Drawing.Size(200, 33);
+            this.buttonGerarCNX.TabIndex = 2;
+            this.buttonGerarCNX.Text = "Gerar CNX";
+            this.buttonGerarCNX.UseVisualStyleBackColor = false;
+            // 
+            // buttonConfigurarCNX
+            // 
+            this.buttonConfigurarCNX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
+            this.buttonConfigurarCNX.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonConfigurarCNX.FlatAppearance.BorderSize = 0;
+            this.buttonConfigurarCNX.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConfigurarCNX.Location = new System.Drawing.Point(0, 100);
+            this.buttonConfigurarCNX.Name = "buttonConfigurarCNX";
+            this.buttonConfigurarCNX.Size = new System.Drawing.Size(200, 33);
+            this.buttonConfigurarCNX.TabIndex = 1;
+            this.buttonConfigurarCNX.Text = "Configurar CNX";
+            this.buttonConfigurarCNX.UseVisualStyleBackColor = false;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.panelLogo.Controls.Add(this.labelLogo);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panelLogo.ForeColor = System.Drawing.Color.White;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(200, 100);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // labelLogo
+            // 
+            this.labelLogo.AutoSize = true;
+            this.labelLogo.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelLogo.ForeColor = System.Drawing.Color.White;
+            this.labelLogo.Location = new System.Drawing.Point(34, 30);
+            this.labelLogo.Name = "labelLogo";
+            this.labelLogo.Size = new System.Drawing.Size(115, 22);
+            this.labelLogo.TabIndex = 0;
+            this.labelLogo.Text = "ReaderAPP";
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.panelTitle.Controls.Add(this.labelTitle);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panelTitle.ForeColor = System.Drawing.Color.White;
+            this.panelTitle.Location = new System.Drawing.Point(200, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(600, 100);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(270, 35);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(41, 17);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "Titulo";
+            // 
+            // panelDesktopPanel
+            // 
+            this.panelDesktopPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktopPanel.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panelDesktopPanel.ForeColor = System.Drawing.Color.White;
+            this.panelDesktopPanel.Location = new System.Drawing.Point(200, 100);
+            this.panelDesktopPanel.Name = "panelDesktopPanel";
+            this.panelDesktopPanel.Size = new System.Drawing.Size(600, 350);
+            this.panelDesktopPanel.TabIndex = 2;
+            // 
+            // FormMainManu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelDesktopPanel);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormMainManu";
+            this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Panel panelDesktopPanel;
+        private System.Windows.Forms.Label labelLogo;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonConfigurarCNX;
+        private System.Windows.Forms.Button buttonGerarCNX;
+    }
+}
+
